2465ce716ae30df3a4baf0dda5716938cedee65a
Fabio-Moscoso <fabio.uszc@gmail.com>
2024-09-16 09:50:53 +0200

    Update flask_start.py

--- Productivizacion/Docker/flask_start.py
+++ Productivizacion/Docker/flask_start.py
@@ -6,12 +6,11 @@ app = Flask(__name__)
 
 # Load model and encoders
 label_encoder = joblib.load('model/label_encoders1.pkl')
-model = joblib.load('model/model_xgb.pkl')
+model = joblib.load('model/model_xgb_optimized.pkl')
 
 def get_label_encoder():
     return label_encoder
 
 def get_model():
     return model
-
 
